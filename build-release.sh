c8aa2b4e454e082fe29860a5f4ff2078f4ab57b9
LinuxFox2509 <dev@linuxfox.eu>
2026-09-07 11:45:47 +0200

    Complete desktop installation support

--- build-release.sh
+++ build-release.sh
@@ -9,9 +9,17 @@ sudo lb clean --purge
 echo "==> Configuring release build..."
 lb config \
   --distribution trixie \
+  --distribution-chroot trixie \
+  --distribution-binary trixie \
+  --parent-distribution trixie \
+  --parent-distribution-chroot trixie \
+  --parent-distribution-binary trixie \
+  --debian-installer-distribution trixie \
+  --parent-debian-installer-distribution trixie \
   --architectures amd64 \
   --binary-images iso-hybrid \
   --archive-areas "main contrib non-free-firmware" \
+  --iso-volume "Fennec Linux 0.1" \
   --bootappend-live "boot=live components username=fennec hostname=fennec" \
   --cache true \
   --cache-packages true \
